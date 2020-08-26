6554287b8459d0dbcd72d2a5d583145223f6ef98
Chris Padilla <cdpadilla42@gmail.com>
2020-08-25 19:08:50 -0500

    Refactor for gql in compenents

--- src/App.js
+++ src/App.js
@@ -1,6 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
+// Writing the query
+const GET_POSTS = gql`
+  query allPosts {
+    posts {
+      stage
+      id
+      title
+      body {
+        text
+      }
+    }
+  }
+`;
+
+function Posts() {
+  const { loading, data } = useQuery(GET_POSTS);
+  if (loading) return 'loading...';
+
+  const { posts } = data;
+
+  console.log(posts);
+
+  return posts.map((post) => (
+    <div key={post.title}>
+      <h3>{post.title}</h3>
+      {/* <p>{post.body.text}</p> */}
+    </div>
+  ));
+}
+
+// ? Running a query outside of react
+// const testQuery = gql`
+//   {
+//     posts {
+//       title
+//       body {
+//         html
+//       }
+//     }
+//   }
+// `;
+
+// client
+//   .query({
+//     query: testQuery,
+//   })
+//   .then((result) => console.log(result));
 
 function App() {
   return (
@@ -19,6 +68,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Posts />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  Switch,
+} from '@apollo/client';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Posts from './posts/Posts';
+
+const client = new ApolloClient({
+  uri: 'https://api-us-east-1.graphcms.com/v2/cke9a14tdu90t01z64k79byow/master',
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route path="/post/:id" component={Posts} />
+        </Switch>
+        {/* <App /> */}
+      </Router>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
