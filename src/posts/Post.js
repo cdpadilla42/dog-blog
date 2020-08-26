6fb4ebaf7331623886f91a321eb2635b4f9e79aa
Chris Padilla <cdpadilla42@gmail.com>
2020-08-26 12:24:40 -0500

    Use variables with Apollo query

--- src/posts/Post.js
+++ src/posts/Post.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
 class Post extends Component {
+  postID = this.props.match.params.id;
+
   render() {
-    return <div>I'm a post</div>;
+    return <PostQuery postID={this.postID} />;
   }
+}
+
+function PostQuery(props) {
+  const GET_POST = gql`
+    query post($id: ID!) {
+      post(where: { id: $id }) {
+        id
+        title
+      }
+    }
+  `;
+
+  console.log('hello from function!', props.postID);
+  const { loading, data } = useQuery(GET_POST, {
+    variables: { id: props.postID },
+  });
+  if (loading) return 'loading...';
+
+  const { post } = data;
+
+  return (
+    <>
+      <h1>Here's your post bruh</h1>
+      <h2>{post.title}</h2>
+    </>
+  );
 }
 
 export default Post;
